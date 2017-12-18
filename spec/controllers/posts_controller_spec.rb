76381e941a0c82bfe9e3c537da67bed752a31381
roshita02 <roshita_shakya@hotmail.com>
2017-12-18 15:58:59 +0545

    Testing create action part1

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -7,11 +7,31 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to render_template :index
     end
   end
-  
+
   describe "GET #new" do
     it "renders the new template" do
       get :new
       expect(response).to render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates a new post" do
+
+      end
+      it "redirects to root_path" do
+
+      end
+    end
+
+    context "with invalid attributes" do
+      it "doesnot create a new post" do
+
+      end
+      it "renders the new template" do
+
+      end
     end
   end
 
